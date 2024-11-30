f35cd41b4f495d8954dff3250f8b59eb24e60317
GwangwooKim <urikokp@kaist.ac.kr>
2024-11-30 19:30:50 +0900

    Add files via upload

---
+++ src/data/copula_generation.R
@@ -0,0 +1,28 @@
+library(copula)
+
+## for copula
+## This refer to https://github.com/yixuan/almond/blob/master/experiments/glmm/01_data.R
+# Dimension
+p = 5
+nsim = 10000
+
+## Clayton copula with theta=2
+set.seed(123)
+cop = claytonCopula(param = 2, dim = p)
+u_cop = rCopula(nsim, cop)
+
+## Gamma marginal distribution
+micdf = function(u) qgamma(u, shape = 2, scale = 1) - 2
+u = micdf(u_cop)
+
+## Simulate GLMM data
+set.seed(123)
+x = 0.2 * matrix(rnorm(nsim * p), nsim, p)
+w = 0.2 * matrix(rnorm(nsim * p), nsim, p)
+beta = runif(p, -2, 2)
+linear = c(x %*% beta) + rowSums(w * u)
+y = matrix(rpois(nsim, exp(linear)) + 0.0, ncol = 1)
+
+data = data.frame(x = x, w = w, y = y, u = u)
+
+# VerbAgg is generated and processed in GLMM.R
